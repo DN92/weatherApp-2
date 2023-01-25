c787e33fe81f3c972d6d35f19b3045b0b95edd64
DN92 <anatoly.tsinker12@gmail.com>
2023-01-25 07:47:00 -0500

    reworked input field and css

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import states from '../../utility/statesDictionary';
 import useLocationFromGeoApi from '../../hooks/useLocationFromGeoApi';
 import styles from './styles.module.css';
+import magnifyingGlass from '../../images/magGlass.webp';
 
 const statesKeys = Object.keys(states);
 const stateFieldBuffer: [string, string] = ['', ''];
@@ -19,6 +21,8 @@ function Home(): React.ReactElement {
   const [fetchError, setFetchError] = useState<string>('');
   const [formError, setFormError] = useState<string>('');
   const [lon, lat, getCoordinates] = useLocationFromGeoApi(setFetchError, setFormError);
+
+  const [userInput, setUserInput] = useState('');
 
   function validateZipCode(): boolean {
     return (userZip.length === 5 && /^(\d+,)*(\d+)$/.test(userZip) && !userZip.includes(' '));
@@ -106,120 +110,138 @@ function Home(): React.ReactElement {
   }, [userCity, userState, userZip, enableZip]);
 
 
-  // JSX BELOW THIS LINE
+  // JSX BELOW THIS LINE--------------------------------------
   return (
     <div className={styles.component_wrapper}>
+      <div className={styles.component}>
+        <button
+          className={styles.use_city_and_state_button}
+          type="button"
+          aria-label={enableZip ? 'switch form to city and state' : 'switch form to use zip code'}
+          onClick={(): void => {
+            setEnableZip((prev) => !prev);
+          }}
+        >
+          {enableZip ? 'Use City and State' : 'Use Zip Code'}
+        </button>
+        <main className={styles.main}>
+          <div className={styles.title}>
+            <h4>Get Your Weather</h4>
+          </div>
+          <div className={styles.search_bar_wrapper}>
+            <input
+              className={styles.search_bar_input}
+              type="text"
+              placeholder="Enter a City or Zipcode"
+              value={userInput}
+              onChange={(e) => {
+                setUserInput(e.target.value);
+              }}
+            />
+            <button
+              type="button"
+              className={[styles.magGlass_btn, 'btn_nostyle'].join(' ')}
+            >
+              <Image
+                className={styles.magGlass}
+                src={magnifyingGlass}
+                alt="magnifying glass submit button"
+                fill
+              />
+            </button>
+          </div>
 
-      <button
-        className={styles.use_city_and_state_button}
-        type="button"
-        aria-label={enableZip ? 'switch form to city and state' : 'switch form to use zip code'}
-        onClick={(): void => {
-          setEnableZip((prev) => !prev);
-        }}
-      >
-        {enableZip ? 'Use City and State' : 'Use Zip Code'}
-      </button>
-      <main className={styles.main}>
-        <div className={styles.title}>
-          <h4>Get Your Weather</h4>
-        </div>
-
-        {enableZip ?
-          (
-            <div>
-              <label htmlFor="zip-code-text-input-id" className={styles.zip_code_text_input_label}>
-                Enter a 5 digit zip code
-                <input
-                  id="zip-code-text-input-id"
-                  className={styles.zip_code_text_input}
-                  type="text"
-                  placeholder="Zip code"
-                  onChange={(event):void => handleZipCodeInput(event)}
-                  value={userZip}
-                />
-              </label>
-            </div>
-          )
-          :
-          (
-            <div>
+          {enableZip ?
+            (
               <div>
-                <label htmlFor="city-text-input">
-                  Location
+                <label htmlFor="zip-code-text-input-id" className={styles.zip_code_text_input_label}>
+                  Enter a 5 digit zip code
                   <input
-                    id="city-text-input"
+                    id="zip-code-text-input-id"
+                    className={styles.zip_code_text_input}
                     type="text"
-                    placeholder="city"
-                    onChange={(event): void => setUserCity(event.target.value)}
-                    value={userCity}
+                    placeholder="Zip code"
+                    onChange={(event):void => handleZipCodeInput(event)}
+                    value={userZip}
                   />
                 </label>
+              </div>
+            )
+            :
+            (
+              <div>
                 <div>
-                  <label htmlFor="select-state-input">
-                    Select State
-                    {/* attempted to keep select menu open. online search recommended 'multiple' attribute but this forces value prop to be an array. */}
-                    {/* TODO: find a way to control the open/close */}
-                    <select
-                      id="select-state-input"
-                      aria-labelledby="select-state-input"
-                      placeholder="select state"
-                      value={userStateAbb}
-                      onKeyDown={(event): void => {
-                        keyDownBuffer(event)();
-                      }}
-                      onChange={(event): void => {
-                        if (event) {
-                          console.log(event.target.value);
-                          setUserStateAbb(event.target.value);
-                        }
-                      }}
-                    >
-                      <option
-                        value=""
-                        label=""
-                        aria-label="none"
-                      />
-                      {statesKeys.map((state, idx) => (
-                        <option
-                          key={idx.toString() + state}
-                          value={state}
-                        >
-                          {state}
-                        </option>
-                      ))}
-                    </select>
-
+                  <label htmlFor="city-text-input">
+                    Location
+                    <input
+                      id="city-text-input"
+                      type="text"
+                      placeholder="city"
+                      onChange={(event): void => setUserCity(event.target.value)}
+                      value={userCity}
+                    />
                   </label>
-
+                  <div>
+                    <label htmlFor="select-state-input">
+                      Select State
+                      {/* attempted to keep select menu open. online search recommended 'multiple' attribute but this forces value prop to be an array. */}
+                      {/* TODO: find a way to control the open/close */}
+                      <select
+                        id="select-state-input"
+                        aria-labelledby="select-state-input"
+                        placeholder="select state"
+                        value={userStateAbb}
+                        onKeyDown={(event): void => {
+                          keyDownBuffer(event)();
+                        }}
+                        onChange={(event): void => {
+                          if (event) {
+                            console.log(event.target.value);
+                            setUserStateAbb(event.target.value);
+                          }
+                        }}
+                      >
+                        <option
+                          value=""
+                          label=""
+                          aria-label="none"
+                        />
+                        {statesKeys.map((state, idx) => (
+                          <option
+                            key={idx.toString() + state}
+                            value={state}
+                          >
+                            {state}
+                          </option>
+                        ))}
+                      </select>
+                    </label>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-
-        <div style={{
-          minHeight: '1rem',
-        }}
-        />
-
-        <div className={styles.get_my_weather_button_wrapper}>
-          <button
-            className={styles.get_my_weather_button}
-            type="button"
-            aria-label="submit form"
-            onClick={(): void => {
-              handleSubmit();
-            }}
-          >
-            Get My Weather
-          </button>
-        </div>
-
-        <div style={{
-          minHeight: '1.25rem',
-        }}
-        />
-      </main>
+            )}
+          <div style={{
+            minHeight: '1rem',
+          }}
+          />
+          <div className={styles.get_my_weather_button_wrapper}>
+            <button
+              className={styles.get_my_weather_button}
+              type="button"
+              aria-label="submit form"
+              onClick={(): void => {
+                handleSubmit();
+              }}
+            >
+              Get My Weather
+            </button>
+          </div>
+          <div style={{
+            minHeight: '1.25rem',
+          }}
+          />
+        </main>
+      </div>
     </div>
   );
 }
